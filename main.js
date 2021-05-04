ee84a2b8a7f5fff998c2f0391e998254386b44b4
Bafana <76040794+Bafana29@users.noreply.github.com>
2021-05-04 18:17:33 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -13,7 +13,23 @@ function take_Snapshot(){
     });
 }
 console.log("ml5 version: ", ml5.version);
-classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/SEF61hNRO/model.json', modelLoaded);
+classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/AbDmJiCpV/model.json', modelLoaded);
 function modelLoaded(){
     console.log("Model Loaded!");
+}
+
+function Identify(){
+    img = document.getElementById("Image");
+    classifier.classify(img, gotResult);
+}
+
+function gotResult(error, results){
+    if(error){
+        console.error(error);
+    }
+    else{
+        console.log(results);
+    }
+    document.getElementById("Object_output").innerHTML = results[0].label;
+document.getElementById("Accuracy").innerHTML = results[0].confidence.toFixed(3);
 }
